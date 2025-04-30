3e4db87e4d94ccc680e6c571f5d3ef9fe398d438
tpct <eslamjackie@outlook.com>
2025-05-01 02:47:15 +0300

    fixing timezone

--- config/app.php
+++ config/app.php
@@ -69,7 +69,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Kathmandu',
+    'timezone' => env('APP_TIMEZONE', 'UTC'),
 
     /*
     |--------------------------------------------------------------------------
